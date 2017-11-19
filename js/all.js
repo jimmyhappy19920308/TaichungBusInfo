4d6959792eb68f9900a785f98bc7d1e0a1592fcc
unknown <>
2017-11-19 22:41:21 +0800

    'four'

--- js/all.js
+++ js/all.js
@@ -4,19 +4,15 @@ var RouteIDs = [];
 var query = '';
 var filterItems;
 const searchBus = document.querySelector('.searchBus');
-console.log(searchBus);
-
-
 
 const xhr = new XMLHttpRequest();
-xhr.open('get', 'http://ptx.transportdata.tw/MOTC/v2/Bus/Route/City/Taichung?$top=300&$format=JSON', true);
+xhr.open('get', 'https://ptx.transportdata.tw/MOTC/v2/Bus/Route/City/Taichung?$top=300&$format=JSON', true);
 xhr.send(null);
 xhr.onload = function () {
   if (xhr.status == 200) { //http狀態碼=200執行程式碼
     data = JSON.parse(xhr.responseText);
-    //console.log(data);
   } else { //http狀態碼為其他值則執行以下程式碼
-    console.log('沒撈到資料');
+    alert('沒撈到資料');
   }
 
   searchBus.addEventListener('input', keywordSearch);
@@ -31,16 +27,16 @@ xhr.onload = function () {
         return el.SubRoutes[0].SubRouteName.Zh_tw.indexOf(query.toUpperCase()) == 0;
       })
     }
-    console.log(filterItems(query));
-
     function updateList(items) {
       const len = items.length;
       var str = '';
       for (var i = 0; i < len; i++) {
-        str += `
+        str += `        
           <li>
-            <span class="Headsign">${items[i].SubRoutes[0].Headsign}</span><br>
-            <span class="RouteId">${items[i].SubRoutes[0].SubRouteName.Zh_tw}</span>
+            <a href="selectbusInfo.html?Zh_tw=${items[i].SubRoutes[0].SubRouteName.Zh_tw}" class="busLink">
+              <span class="Headsign">${items[i].SubRoutes[0].Headsign}</span><br>
+              <span class="RouteId">${items[i].SubRoutes[0].SubRouteName.Zh_tw}</span>
+            </a>
           </li>
         `;
       }
@@ -49,24 +45,5 @@ xhr.onload = function () {
     //console.log(data);
     updateList(filterItems(query));
   }
-
-
-
-  /*   function updateList(items) {
-      const len = items.length;
-      var str = '';
-      for (var i = 0; i < len; i++) {
-        str += `
-          <li>
-            <span class="Headsign">${items[i].SubRoutes[0].Headsign}</span><br>
-            <span class="RouteId">${items[i].RouteID}</span>
-          </li>
-        `;
-      }
-      list.innerHTML = str;
-    }
-    //console.log(data);
-    updateList(data); */
-
-
 }
+
